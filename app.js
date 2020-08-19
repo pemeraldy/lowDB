a51964d692897955779a975711e8c1327033eccf
unknown <pemeraldy@gmail.com>
2020-08-19 08:39:36 +0100

    added few endpoint inregard to categories

--- app.js
+++ app.js
@@ -7,25 +7,76 @@ const bodyParser = require('body-parser')
 
 const db = lowDb(new fileSync('store.json'))
 
-db.defaults({ store: [] }).write()
+db.defaults({ products: [] }).write()
 
 const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
-app.get('/store', (req, res) => {
-    const data = db.get('store').value()
+app.get('/products', (req, res) => {
+    const data = db.get('products').value()
     return res.json(data)
 })
 
-app.post('/store/new', (req, res) => {
+// get all male watches
+app.get('/products/male', (req, res) => {
+    const data = db.get('products').filter(prod => {
+        return prod.gender == "male"
+    }).value()
+    console.log(data)
+    return res.json(data)
+})
+
+// get all female watches
+app.get('/products/female', (req, res) => {
+    const data = db.get('products').filter(prod => {
+        return prod.gender == "female"
+    }).value()
+    console.log(data)
+    return res.json(data)
+})
+
+// get a specific watch brand
+app.get('/products/female', (req, res) => {
+    const data = db.get('products').filter(prod => {
+        return prod.brand == "Hoblot"
+    }).value()
+    console.log(data)
+    return res.json(data)
+})
+
+app.post('/products/new', (req, res) => {
     const product = req.body
-    db.get('store').push({
+    console.log(product)
+    db.get('products').push({
         ...product, id: nanoid()
     }).write()
     res.json({
         success: true
     })
+    // res.redirect('/products')
+})
+
+app.get('/categories', (req, res) => {
+    const cats = db.get('categories').value()
+    return res.json(cats)
+})
+app.post('/categories/new', (req, res) => {
+    const category = req.body
+    // const value = Object.values(category)
+    db.get('categories').push(category.cat).write()
+    res.json({
+        success: true
+    })
+})
+
+app.get('/categories/adults', (req, res) => {
+    const adults = db.get('categories')
+        .find('adults')
+        .value()
+    console.log(adults)
+
+    return res.send(adults)
 })
 
 const PORT = 4000
